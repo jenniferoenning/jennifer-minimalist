cc1f14a493bc255abcea24e841d374fb2522beeb
jenniferoenning <oenningstangjennifer@gmail.com>
2021-03-31 13:52:34 -0300

    added socials

--- src/components/About.vue
+++ src/components/About.vue
@@ -2,10 +2,11 @@
   <div class="container-about">
       <div class="row mb-3">
           <div class="col-sm-7">
-              <h1 class="text-left">{{ name }}</h1>
-              <h3 class="text-left about-me">{{ worker }}</h3>
+            <h1 class="text-left">{{ name }}</h1>
+            <h3 class="text-left about-me">{{ worker }}</h3>
+            <Social />
           </div>
-          <div class="col-sm-4">
+          <div class="col-sm-5">
               <img class="w-100" src="../assets/logo.png" alt="Draw Jennifer">
           </div>
       </div>
@@ -17,15 +18,20 @@
 
 <script>
 
+import Social from './Social.vue'
+
 export default {
-  name: 'About',
-  data () {
-      return {
-        name: "JENNIFER OENNING",
-        worker: "web developer & content creator",
-        message: "Olá seja bem vindo ao meu cantinho! Me chamo Jennifer Oenning, tenho 20 anos e eu sou extremamente apaixonada por tecnologia e games, atualmente estou no setor de desenvolvimento web como full stack na empresa Bw2 Tecnologia, também sou criadora de conteúdo na plataforma da Twitch e Youtube, construi este site pra vocês conhecerem um pouquinho de mim e conhecer meus projetos, eles são totalmente códigos abertos para quaisquer opiniões ou mudanças, quero criar aqui algumas matérias de tecnologias, tutoriais, espero que vocês gostem e se quiser me seguir nas redes sociais vou deixar logo a baixo :D",
+    name: 'About',
+    components: {
+        Social
+    },
+    data () {
+        return {
+            name: "JENNIFER OENNING",
+            worker: "web developer & content creator",
+            message: "Olá seja bem vindo ao meu cantinho! Me chamo Jennifer Oenning, tenho 20 anos e eu sou extremamente apaixonada por tecnologia e games, atualmente estou no setor de desenvolvimento web como full stack na empresa Bw2 Tecnologia, também sou criadora de conteúdo na plataforma da Twitch e Youtube, construi este site pra vocês conhecerem um pouquinho de mim e conhecer meus projetos, eles são totalmente códigos abertos para quaisquer opiniões ou mudanças, quero criar aqui algumas matérias de tecnologias, tutoriais, espero que vocês gostem e se quiser me seguir nas redes sociais vou deixar logo a baixo :D",
+        }
     }
-  }
 }
 </script>
 
---
+++ src/components/Social.vue
@@ -0,0 +1,81 @@
+<template>
+    <div class="d-flex mt-3">
+        <ul class="sociais-bio">
+            <li>
+                <a :href="urlGithub">
+                    <img class="bioicon-github" src="../assets/github.svg" alt="Github icon">
+                </a>
+            </li>
+            <li>
+                <a :href="urlLinkedin">
+                    <img class="bioicon-linkedin" src="../assets/linkedin.svg" alt="Linkedin icon">
+                </a>
+            </li>
+            <li>
+                <a :href="urlTwitter">
+                    <img class="bioicon-twitter" src="../assets/twitter.svg" alt="Twitter icon">
+                </a>
+            </li>
+            <li>
+                <a :href="urlTwitch">
+                    <img class="bioicon-twitch" src="../assets/twitch.svg" alt="Twitch icon">
+                </a>
+            </li>
+            <li>
+                <a :href="urlInstagram">
+                    <img class="bioicon-instagram" src="../assets/instagram.svg" alt="Instagram icon">
+                </a>
+            </li>
+            <li>
+                <a :href="urlYoutube">
+                    <img class="bioicon-youtube" src="../assets/youtube.svg" alt="Youtube icon">
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Social',
+  data () {
+      return {
+        urlGithub: "https://github.com/jenniferoenning",
+        urlLinkedin: "https://linkedin.com/in/jennifer-stang-oenning/",
+        urlTwitter: "https://twitter.com/JenniferOenning",
+        urlTwitch: "https://twitch.tv/jenni",
+        urlInstagram: "https://instagram.com/jenniferoenning/",
+        urlYoutube: "https://youtube.com/c/jenniferoenningg"
+    }
+  }
+}
+</script>
+
+<style lang="css" scoped>
+    
+    .sociais-bio {
+        list-style: none;
+        display: inline-flex;
+        padding: 0px;
+    }
+    
+    .bioicon-github,
+    .bioicon-linkedin,
+    .bioicon-twitter,
+    .bioicon-twitch,
+    .bioicon-instagram,
+    .bioicon-youtube {
+        filter: invert(41%) sepia(107%) saturate(0%) hue-rotate(87deg) brightness(88%) contrast(42%);
+        width: 24px;
+        height: 30px;
+        margin-right: 15px;
+    }
+    .bioicon-github:hover,
+    .bioicon-linkedin:hover,
+    .bioicon-twitter:hover,
+    .bioicon-twitch:hover,
+    .bioicon-instagram:hover,
+    .bioicon-youtube:hover {
+        filter: invert(41%) sepia(107%) saturate(0%) hue-rotate(87deg) brightness(40%) contrast(42%);
+    }
+</style>
